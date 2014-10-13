5d44b0c600e051aa0b497881aec277b227b7a150
lyz <lyz@lyz-PC>
2014-10-13 14:55:59 +0800

    增加3

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,9 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("创建test");
+        
+        Lyz_Class lyz = new Lyz_Class();
+        lyz.print();
     }
     
 }
